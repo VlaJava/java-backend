0ad9fcb5f49b11fc8c5e6b22dec0900743072e7c
EmanuelErnesto <emanuel.earego@gmail.com>
2025-07-22 10:10:20 -0300

    raw(security): add role entity model

--- src/main/java/com/avanade/decolatech/viajava/domain/model/Usuario.java
+++ src/main/java/com/avanade/decolatech/viajava/domain/model/Usuario.java
@@ -1,10 +1,17 @@
 package com.avanade.decolatech.viajava.domain.model;
 
+import com.avanade.decolatech.viajava.domain.model.enums.UsuarioRole;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -15,7 +22,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @ToString
 @Builder
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -36,7 +43,41 @@ public class Usuario {
     @Column(name = "ATIVO", nullable = false)
     private boolean ativo;
 
+    @Column(name = "DATA_NASC", nullable = false)
+    private LocalDate dataNasc;
+
+    @Column(name = "IMAGEM_PERFIL")
+    private String imagemPerfil;
+
+    @OneToOne(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private Role role;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private List<Documento> documentos;
+
     @CreationTimestamp
     @Column(name = "DATA_CADASTRO", nullable = false)
     private LocalDateTime dataCadastro;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role.getUsuarioRole() == UsuarioRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+
+        return List.of(new SimpleGrantedAuthority("ROLE_CLIENTE"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.ativo;
+    }
 }
