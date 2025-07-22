5aa9475478d9feff38a22515af77626fd36f13e3
EmanuelErnesto <emanuel.earego@gmail.com>
2025-07-22 10:13:35 -0300

    feat(security): add auth service

---
+++ src/main/java/com/avanade/decolatech/viajava/service/auth/AuthService.java
@@ -0,0 +1,58 @@
+package com.avanade.decolatech.viajava.service.auth;
+
+import com.avanade.decolatech.viajava.domain.dtos.request.LoginResponse;
+
+import com.avanade.decolatech.viajava.domain.model.Usuario;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+
+@Service
+public class AuthService {
+
+    private final JwtEncoder jwtEncoder;
+    private final JwtDecoder jwtDecoder;
+    private final Logger logger = LoggerFactory.getLogger(AuthService.class);
+
+    public AuthService(JwtEncoder jwtEncoder, JwtDecoder jwtDecoder) {
+        this.jwtEncoder = jwtEncoder;
+        this.jwtDecoder = jwtDecoder;
+    }
+
+    public LoginResponse generateToken(Usuario user) {
+        var now = Instant.now();
+        var expiresIn = 300L;
+
+        String roleName = user.getRole().getUsuarioRole().name();
+
+        var claims = JwtClaimsSet.builder()
+                .issuer("viajava-backend")
+                .subject(user.getId().toString())
+                .expiresAt(now.plusSeconds(expiresIn * 10 * 60))
+                .claim("scope", roleName)
+                .claim("username", user.getEmail())
+                .build();
+
+        var token = jwtEncoder
+                .encode(JwtEncoderParameters.from(claims))
+                .getTokenValue();
+
+        return new LoginResponse(token, 5L);
+    }
+
+    public String validateToken(String token) {
+        try {
+            return this.jwtDecoder.decode(token).getClaimAsString("username");
+        } catch (JwtException e) {
+            this.logger.error("[AuthService validateToken] - Falha ao validar token: {}", e.getMessage());
+
+            return null;
+        }
+
+    }
+
+}
