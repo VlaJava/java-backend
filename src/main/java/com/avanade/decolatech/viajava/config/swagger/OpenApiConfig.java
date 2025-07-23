19b48fb6a138624bd769c36fbbbea81aecb24ec2
EmanuelErnesto <emanuel.earego@gmail.com>
2025-07-23 11:37:31 -0300

    raw(swagger): add swagger configuration file

---
+++ src/main/java/com/avanade/decolatech/viajava/config/swagger/OpenApiConfig.java
@@ -0,0 +1,39 @@
+package com.avanade.decolatech.viajava.config.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("security", securityScheme()))
+                .info(
+                        new Info()
+                                .title("Rest API - ViaJava")
+                                .description("API desenvolvida para gerenciamento de reservas e pacotes de viagens.")
+                                .version("v1")
+                                .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                                .contact(new Contact().name("Viajava").email("corporate.viajava@gmail.com"))
+
+                );
+    }
+
+    private SecurityScheme securityScheme(){
+        return new SecurityScheme()
+                .description("Insert a valid bearer token for continue")
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("security");
+    }
+}
