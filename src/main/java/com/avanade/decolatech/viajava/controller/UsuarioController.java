db8800422a5eaf597b12180771ae6676d84c8c8f
EmanuelErnesto <emanuel.earego@gmail.com>
2025-07-23 09:54:57 -0300

    docs(usuario): add swagger annotations in usuario controller

--- src/main/java/com/avanade/decolatech/viajava/controller/UsuarioController.java
+++ src/main/java/com/avanade/decolatech/viajava/controller/UsuarioController.java
@@ -4,10 +4,16 @@ import com.avanade.decolatech.viajava.domain.dtos.request.CreateUsuarioRequest;
 import com.avanade.decolatech.viajava.domain.dtos.response.CreateUsuarioResponse;
 import com.avanade.decolatech.viajava.domain.dtos.response.PaginatedUsuarioResponse;
 import com.avanade.decolatech.viajava.domain.dtos.response.UsuarioResponse;
+import com.avanade.decolatech.viajava.domain.exception.ApplicationException;
 import com.avanade.decolatech.viajava.domain.mapper.UsuarioMapper;
 import com.avanade.decolatech.viajava.domain.model.Usuario;
 import com.avanade.decolatech.viajava.domain.model.enums.UsuarioRole;
 import com.avanade.decolatech.viajava.service.usuario.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.data.domain.Page;
@@ -21,6 +27,8 @@ import org.springframework.core.io.Resource;
 import java.io.IOException;
 import java.util.UUID;
 
+
+@Tag(name = "Usuarios", description = "Endpoints para operações CRUD envolvendo criação, busca, deleção e atualização de um usuário.")
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
@@ -47,6 +55,15 @@ public class UsuarioController {
         this.usuarioMapper = usuarioMapper;
     }
 
+    @Operation(summary = "Cria um novo usuário com role cliente.", description = "Recurso para criar um novo usuário com role cliente.",
+    responses = {
+            @ApiResponse(responseCode = "201", description = "Usuário Criado com sucesso.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CreateUsuarioResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Credencial já utilizada por outro usuário.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationException.class))),
+            @ApiResponse(responseCode = "422", description = "O dado passado na requisição é inválido.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationException.class)))
+    })
     @PostMapping
     public ResponseEntity<CreateUsuarioResponse> criarUsuario(@RequestBody @Valid CreateUsuarioRequest request) {
         CreateUsuarioResponse response = this.createUsuarioService.criarUsuario(request, UsuarioRole.CLIENTE);
@@ -54,6 +71,15 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "Cria um novo usuário com role admin.", description = "Recurso para criar um novo usuário com role admin.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Admin Criado com sucesso.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CreateUsuarioResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "Credencial já utilizada por outro usuário.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationException.class))),
+                    @ApiResponse(responseCode = "422", description = "O dado passado na requisição é inválido.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationException.class)))
+            })
     @PostMapping("/admin")
     public ResponseEntity<CreateUsuarioResponse> criarUsuarioAdmin(@RequestBody @Valid CreateUsuarioRequest request) {
         CreateUsuarioResponse response = this.createUsuarioService.criarUsuario(request, UsuarioRole.ADMIN);
@@ -61,6 +87,15 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "Busca um usuário pelo id.", description = "Recurso para buscar um novo usuário existente através do ID.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Usuário retornado com sucesso.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CreateUsuarioResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "Usuário não encontrado.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationException.class))),
+                    @ApiResponse(responseCode = "422", description = "O id passado na requisição é inválido.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationException.class)))
+            })
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioResponse> getUsuarioById(@PathVariable("id") UUID id) {
         Usuario response = this.getUsuarioByIdService.execute(id);
@@ -68,6 +103,11 @@ public class UsuarioController {
         return ResponseEntity.ok(usuarioMapper.toUsuarioResponse(response));
     }
 
+    @Operation(summary = "Retorna uma lista de usuários paginados", description = "Recurso para buscar uma lista de usuários com paginação",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Usuários retornados com sucesso.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CreateUsuarioResponse.class))),
+            })
     @GetMapping
     public ResponseEntity<PaginatedUsuarioResponse> getTodosUsuarios(
             @RequestParam(defaultValue = "0") @PositiveOrZero Integer page,
@@ -78,6 +118,15 @@ public class UsuarioController {
         return ResponseEntity.ok(usuarioMapper.toPaginatedUsuarioResponse(response));
     }
 
+    @Operation(summary = "Deleta um usuário.", description = "Recurso para deletar um novo usuário existente através do ID.",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Usuário deletado com sucesso.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CreateUsuarioResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "Usuário não encontrado.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationException.class))),
+                    @ApiResponse(responseCode = "422", description = "O id passado na requisição é inválido.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationException.class)))
+            })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUsuario(@PathVariable("id") UUID id) {
         this.deleteUsuarioService.execute(id);
@@ -85,6 +134,15 @@ public class UsuarioController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Atualiza a imagem de perfil do usuário.", description = "Recurso para atualizar a imagem de perfil do usuário.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Imagem atualizada com sucesso.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CreateUsuarioResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "Usuário não encontrado.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationException.class))),
+                    @ApiResponse(responseCode = "422", description = "O id passado na requisição é inválido.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationException.class)))
+            })
     @PatchMapping("/{id}/update-image")
     public ResponseEntity<Resource> uploadImage(@RequestParam("file") MultipartFile file, @PathVariable("id") UUID id) throws IOException {
         Resource response = this.updateUsuarioImagemService.updateProfileImage(file, id);
