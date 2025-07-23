fbcb0890946bb6a4baf3f68e6092ea34d14568fc
EmanuelErnesto <emanuel.earego@gmail.com>
2025-07-23 09:56:11 -0300

    raw(usuario): add imagem perfil property to usuario response

--- src/main/java/com/avanade/decolatech/viajava/domain/dtos/response/UsuarioResponse.java
+++ src/main/java/com/avanade/decolatech/viajava/domain/dtos/response/UsuarioResponse.java
@@ -3,6 +3,6 @@ package com.avanade.decolatech.viajava.domain.dtos.response;
 import java.util.UUID;
 
 public record UsuarioResponse(
-        UUID id, String nome, String email, String telefone, boolean ativo
+        UUID id, String nome, String email, String telefone, String imagemPerfil, boolean ativo
 ) {
 }
