913397a30251c8e5ad4c3d12c9d3619394572640
filipe4ndrade <filipeandrade000@gmail.com>
2025-07-24 17:43:51 -0300

    feat: Criação da entidade Packages

---
+++ src/main/java/com/avanade/decolatech/viajava/domain/model/Package.java
@@ -0,0 +1,53 @@
+package com.avanade.decolatech.viajava.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PACKAGES")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class Package {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "ID")
+    private UUID id;
+
+    @Column(name = "TITLE", nullable = false, length = 100)
+    private String title;
+
+    @Column(name = "SOURCE", nullable = false, length = 100)
+    private String source;
+
+    @Column(name = "DESTINATION", nullable = false, length = 100)
+    private String destination;
+
+    @Column(name = "DESCRIPTION", nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "IMAGE_URL", length = 255)
+    private String imageUrl;
+
+    @Column(name = "PRICE", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "TRAVELER_LIMIT", nullable = false)
+    private int travelerLimit;
+
+    @Column(name = "START_DATE", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "END_DATE", nullable = false)
+    private LocalDate endDate;
+
+    @Column(name = "AVALIABLE", nullable = false)
+    private boolean available;
+}
