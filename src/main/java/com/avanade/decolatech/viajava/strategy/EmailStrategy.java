e9b43052bdd78f3b3fdb7723bbebf7365245c543
EmanuelErnesto <emanuel.earego@gmail.com>
2025-07-25 00:22:10 -0300

    refactor(email): change entity parameter name in factory

--- src/main/java/com/avanade/decolatech/viajava/strategy/EmailStrategy.java
+++ src/main/java/com/avanade/decolatech/viajava/strategy/EmailStrategy.java
@@ -1,12 +1,12 @@
 package com.avanade.decolatech.viajava.strategy;
 
-import com.avanade.decolatech.viajava.domain.model.Usuario;
+import com.avanade.decolatech.viajava.domain.model.User;
 import jakarta.mail.MessagingException;
 
 import java.io.UnsupportedEncodingException;
 
 public interface EmailStrategy {
-    void sendEmail(Usuario usuario) throws MessagingException, UnsupportedEncodingException;
+    void sendEmail(User user) throws MessagingException, UnsupportedEncodingException;
 
     EmailType getEmailType();
 }
