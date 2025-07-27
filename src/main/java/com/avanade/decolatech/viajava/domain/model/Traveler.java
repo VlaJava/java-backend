ca7645ce1c95e66972178c6653e172c428031534
filipe4ndrade <filipeandrade000@gmail.com>
2025-07-27 20:44:49 -0300

    feat: Criação de

---
+++ src/main/java/com/avanade/decolatech/viajava/domain/model/Traveler.java
@@ -0,0 +1,35 @@
+package com.avanade.decolatech.viajava.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_TRAVELERS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Traveler {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "BOOKING_ID", nullable = false)
+    private Booking booking;
+
+    @Column(name = "NAME", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "DOCUMENT", nullable = false, length = 20)
+    private String document;
+
+    @Column(name = "BIRTHDATE", nullable = false)
+    private LocalDate birthdate;
+
+}
