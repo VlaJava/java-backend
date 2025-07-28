0a9f752156021996c208a5f0a9af27f736aa1963
EmanuelErnesto <emanuel.earego@gmail.com>
2025-07-28 16:44:38 -0300

    raw(payment): add enums of payment status and method

---
+++ src/main/java/com/avanade/decolatech/viajava/domain/model/enums/PaymentMethod.java
@@ -0,0 +1,10 @@
+package com.avanade.decolatech.viajava.domain.model.enums;
+
+public enum PaymentMethod {
+    CREDIT_CARD,
+    DEBIT_CARD,
+    PIX,
+    BOLETO,
+    ACCOUNT_MONEY,
+    OTHER
+}
