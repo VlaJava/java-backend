cb5cd0c32fad78f28b0ccb735576f7e9be4fa0e7
EmanuelErnesto <emanuel.earego@gmail.com>
2025-07-29 21:33:35 -0300

    feat(payments): email sending after payment creation

--- src/main/java/com/avanade/decolatech/viajava/service/payment/CreatePreferenceService.java
+++ src/main/java/com/avanade/decolatech/viajava/service/payment/CreatePreferenceService.java
@@ -2,19 +2,23 @@ package com.avanade.decolatech.viajava.service.payment;
 
 import com.avanade.decolatech.viajava.client.MercadoPagoClient;
 import com.avanade.decolatech.viajava.domain.dtos.request.payment.CreatePreferenceRequest;
-import com.avanade.decolatech.viajava.domain.dtos.response.payment.CreatePreferenceResponse;
+
 import com.avanade.decolatech.viajava.domain.exception.BusinessException;
+import com.avanade.decolatech.viajava.domain.exception.PaymentGatewayException;
 import com.avanade.decolatech.viajava.domain.exception.ResourceNotFoundException;
 import com.avanade.decolatech.viajava.domain.model.Booking;
 import com.avanade.decolatech.viajava.domain.model.Package;
 import com.avanade.decolatech.viajava.domain.model.User;
+import com.avanade.decolatech.viajava.domain.model.enums.BookingStatus;
 import com.avanade.decolatech.viajava.domain.repository.BookingRepository;
+import com.avanade.decolatech.viajava.service.email.EmailService;
+import com.avanade.decolatech.viajava.strategy.EmailType;
 import com.avanade.decolatech.viajava.utils.PaymentExceptionMessages;
 import com.avanade.decolatech.viajava.utils.properties.ApplicationProperties;
-import com.mercadopago.exceptions.MPApiException;
-import com.mercadopago.exceptions.MPException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,21 +27,23 @@ import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Service
-public class CreatePaymentService {
+public class CreatePreferenceService {
 
-    private final Logger logger = LoggerFactory.getLogger(CreatePaymentService.class);
+    private final Logger logger = LoggerFactory.getLogger(CreatePreferenceService.class);
     private final BookingRepository bookingRepository;
     private final MercadoPagoClient mercadoPagoClient;
     private final ApplicationProperties properties;
+    private final EmailService emailService;
 
-    public CreatePaymentService(BookingRepository bookingRepository, MercadoPagoClient mercadoPagoClient, ApplicationProperties properties) {
+    public CreatePreferenceService(BookingRepository bookingRepository, MercadoPagoClient mercadoPagoClient, ApplicationProperties properties, EmailService emailService) {
         this.bookingRepository = bookingRepository;
         this.mercadoPagoClient = mercadoPagoClient;
         this.properties = properties;
+        this.emailService = emailService;
     }
 
     @Transactional
-    public CreatePreferenceResponse createPayment(UUID bookingId, UUID userId) throws MPException, MPApiException {
+    public void createPayment(UUID bookingId, UUID userId) {
         Booking booking = this.bookingRepository
                 .findById(bookingId)
                 .orElseThrow(() -> new ResourceNotFoundException("Booking not found"));
@@ -45,6 +51,7 @@ public class CreatePaymentService {
         User user = booking.getUser();
 
         this.validateIfUserIsActive(user);
+        this.validateBookingStatus(booking.getBookingStatus());
         this.validateIfPayerIsValid(userId, user.getId());
 
         Package travelPackage = booking.getTravelPackage();
@@ -53,9 +60,21 @@ public class CreatePaymentService {
 
         String orderNumber = this.generateOrderNumber(userId.toString());
 
+        this.logger.info("Preference created for order number: {} ", orderNumber);
 
+        booking.setOrderNumber(orderNumber);
 
-        return this.mercadoPagoClient.createPreference(request, orderNumber);
+        this.bookingRepository.save(booking);
+
+        this.mercadoPagoClient.createPreference(request, orderNumber)
+                .thenAccept(response -> {
+                    this.logger.info("Mercado pago redirect url: {} ", response.redirectUrl());
+
+                    this.emailService.sendEmail(user, EmailType.PAYMENT_REQUEST_EMAIL,response.redirectUrl());
+                })
+                .exceptionally(ex -> {
+                    throw new PaymentGatewayException(ex.getMessage());
+                });
     }
 
     /**
@@ -73,6 +92,16 @@ public class CreatePaymentService {
     private void validateIfUserIsActive(User user) {
         if(!user.isActive()) {
             throw new BusinessException(PaymentExceptionMessages.USER_IS_INACTIVE);
+        }
+    }
+
+    private void validateBookingStatus(BookingStatus status) {
+        if(status.equals(BookingStatus.CANCELLED)) {
+            throw new BusinessException(PaymentExceptionMessages.BOOKING_CANCELLED);
+        }
+
+        if(status.equals(BookingStatus.CONFIRMED)) {
+            throw new BusinessException(PaymentExceptionMessages.BOOKING_ALREADY_CONFIRMED);
         }
     }
 
@@ -102,8 +131,6 @@ public class CreatePaymentService {
     }
 
     private CreatePreferenceRequest.BackUrlsDTO buildBackUrls() {
-        this.logger.info("Success url {}", this.properties.getGatewaySuccessUrl());
-
         return new CreatePreferenceRequest.BackUrlsDTO(
                 this.properties.getGatewaySuccessUrl(),
                 this.properties.getGatewayFailUrl(),
