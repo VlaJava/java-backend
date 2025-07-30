6b52f12f6be19029231758c30b1275333374b179
EmanuelErnesto <emanuel.earego@gmail.com>
2025-07-29 21:29:00 -0300

    feat(bookings): add find by order number method in repository

--- src/main/java/com/avanade/decolatech/viajava/domain/repository/BookingRepository.java
+++ src/main/java/com/avanade/decolatech/viajava/domain/repository/BookingRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface BookingRepository extends JpaRepository<Booking, UUID> {
 
     Page<Booking> findAllByUserId(UUID userId, Pageable pageable);
+    Optional<Booking> findByOrderNumber(String orderNumber);
 }
