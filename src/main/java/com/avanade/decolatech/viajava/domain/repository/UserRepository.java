147e18e8836e41d3949942ecddd9d3792df4df0c
EmanuelErnesto <emanuel.earego@gmail.com>
2025-07-30 10:53:12 -0300

    raw(users): add query to get user payments in repository

--- src/main/java/com/avanade/decolatech/viajava/domain/repository/UserRepository.java
+++ src/main/java/com/avanade/decolatech/viajava/domain/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.avanade.decolatech.viajava.domain.repository;
 
+import com.avanade.decolatech.viajava.domain.dtos.response.payment.PaymentUserResponse;
 import com.avanade.decolatech.viajava.domain.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +20,15 @@ public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificat
     @Query("SELECT U FROM User U WHERE U.email = :username")
     UserDetails findByUsername(String username);
 
+    @Query("SELECT new com.avanade.decolatech.viajava.domain.dtos.response.payment.PaymentUserResponse(" +
+            "P.id, " +
+            "P.paymentMethod, " +
+            "P.amount, " +
+            "P.paymentDate, " +
+            "P.paymentStatus) " +
+            "FROM User U " +
+            "LEFT JOIN Booking B ON U.id = B.user " +
+            "RIGHT JOIN PaymentEntity P ON B.id = P.booking " +
+            "WHERE U.id = :userId")
+    Page<PaymentUserResponse> findAllPaymentsByUserId(UUID userId, Pageable pageable);
 }
