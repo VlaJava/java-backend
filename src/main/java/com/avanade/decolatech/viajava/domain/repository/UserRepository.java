259146370d478d2ed138132567f9091794ae716e
EmanuelErnesto <emanuel.earego@gmail.com>
2025-07-29 21:30:01 -0300

    raw(users): add jpa specification in repository interface

--- src/main/java/com/avanade/decolatech/viajava/domain/repository/UserRepository.java
+++ src/main/java/com/avanade/decolatech/viajava/domain/repository/UserRepository.java
@@ -2,13 +2,14 @@ package com.avanade.decolatech.viajava.domain.repository;
 
 import com.avanade.decolatech.viajava.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
 
     Optional<User> findByEmail(String email);
     Optional<User> findByPhone(String telefone);
