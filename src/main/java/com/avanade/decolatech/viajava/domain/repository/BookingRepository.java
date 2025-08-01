5943b84ef72e91b55ffbe7c65ab8e48063c245ab
EmanuelErnesto <emanuel.earego@gmail.com>
2025-08-01 18:15:50 -0300

    feat(bookings): add find by package and user id method in repository

--- src/main/java/com/avanade/decolatech/viajava/domain/repository/BookingRepository.java
+++ src/main/java/com/avanade/decolatech/viajava/domain/repository/BookingRepository.java
@@ -4,6 +4,7 @@ import com.avanade.decolatech.viajava.domain.model.Booking;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -11,5 +12,9 @@ import java.util.UUID;
 public interface BookingRepository extends JpaRepository<Booking, UUID> {
 
     Page<Booking> findAllByUserId(UUID userId, Pageable pageable);
+
     Optional<Booking> findByOrderNumber(String orderNumber);
+
+    @Query("SELECT B FROM Booking B WHERE B.travelPackage.id = :packageId AND B.user.id = :userId")
+    Optional<Booking> findByPackageAndUserId(UUID packageId, UUID userId);
 }
