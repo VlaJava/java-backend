eaa6d2b89b50734259fc96dd5b2160c0aa42bfc0
EmanuelErnesto <emanuel.earego@gmail.com>
2025-08-04 09:23:51 -0300

    raw: add annotation in upload img request

--- src/main/java/com/avanade/decolatech/viajava/domain/dtos/request/user/UploadImageRequest.java
+++ src/main/java/com/avanade/decolatech/viajava/domain/dtos/request/user/UploadImageRequest.java
@@ -1,6 +1,7 @@
 package com.avanade.decolatech.viajava.domain.dtos.request.user;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 @NoArgsConstructor
 public class UploadImageRequest {
     @Schema(type = "string", format = "binary", description = "Arquivo de imagem JPEG ou PNG")
+    @NotNull
     private MultipartFile file;
 }
 
