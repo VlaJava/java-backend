c352e9bb224866fbe3ceae9e3fcf07aa63d56136
filipe4ndrade <filipeandrade000@gmail.com>
2025-08-05 07:46:20 -0300

    feat: novo endpoint do super avanildo

--- src/main/java/com/avanade/decolatech/viajava/controller/GeminiChatController.java
+++ src/main/java/com/avanade/decolatech/viajava/controller/GeminiChatController.java
@@ -2,7 +2,8 @@ package com.avanade.decolatech.viajava.controller;
 
 import com.avanade.decolatech.viajava.domain.dtos.request.geminiChat.GeminiChatRequest;
 import com.avanade.decolatech.viajava.domain.dtos.response.geminiChat.BotResponse;
-import com.avanade.decolatech.viajava.service.geminiChat.GeminiChatService;
+import com.avanade.decolatech.viajava.service.geminiChat.superAvanildo.GeminiChatSuperAvanildoService;
+import com.avanade.decolatech.viajava.service.geminiChat.avanildo.GeminiChatAvanildoService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,16 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/chat")
 public class GeminiChatController {
 
-    private final GeminiChatService geminiChatService;
+    private final GeminiChatAvanildoService geminiChatAvanildoService;
+    private final GeminiChatSuperAvanildoService geminiChatSuperAvanildoService;
 
-    public GeminiChatController(GeminiChatService geminiChatService) {
-        this.geminiChatService = geminiChatService;
+    public GeminiChatController(GeminiChatAvanildoService geminiChatAvanildoService, GeminiChatSuperAvanildoService geminiChatSuperAvanildoService) {
+        this.geminiChatSuperAvanildoService = geminiChatSuperAvanildoService;
+        this.geminiChatAvanildoService = geminiChatAvanildoService;
     }
+
+    public record DataChatResponse(String text) {}
 
     @PostMapping
     public ResponseEntity<BotResponse> handleChatMessage(@Valid @RequestBody GeminiChatRequest request) throws Exception {
 
-        BotResponse response = geminiChatService.getChatResponse(request.message(), request.history());
+        BotResponse response = geminiChatAvanildoService.getChatResponse(request.message(), request.history());
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/super")
+    public ResponseEntity<DataChatResponse> handleDataChatMessage(@Valid @RequestBody GeminiChatRequest request) throws Exception {
+        DataChatResponse response = geminiChatSuperAvanildoService.getChatResponse(request.message(), request.history());
         return ResponseEntity.ok(response);
     }
 }
